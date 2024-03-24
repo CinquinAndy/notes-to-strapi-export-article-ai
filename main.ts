fc707ce2c0d81371fc1243c08e7d15c9c10949e6
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-24 20:03:53 +0100

    ✨ Add Editor interface, settings interface, and default settings
    Added interfaces for plugin settings and default settings. Updated ribbon icon logic to include getting current markdown file content. Created a new setting tab for configuring plugin aspects.

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import {
+	App,
+	Editor,
 	MarkdownView,
 	Modal,
 	Notice,
@@ -7,73 +9,45 @@ import {
 	Setting,
 } from 'obsidian'
 
+// Remember to rename these classes and interfaces!
+
+interface MyPluginSettings {
+	mySetting: string
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default',
+}
+
 export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings
+
 	async onload() {
 		await this.loadSettings()
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', evt => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!')
-		})
+		const ribbonIconEl = this.addRibbonIcon(
+			'dice',
+			'Sample Plugin',
+			(evt: MouseEvent) => {
+				// todo add the logic here, get the current markdown file, get the content, and log it
+				const activeView = this.app.workspace.getActiveViewOfType(MarkdownView)
+				if (!activeView) {
+					new Notice('No active Markdown view')
+					return
+				}
+				const editor = activeView.editor
+				const content = editor.getValue()
+				console.log(content)
+				console.log('content')
+				new Notice('This is a notice!')
+			}
+		)
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class')
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem()
-		statusBarItemEl.setText('Status Bar Text')
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open()
-			},
-		})
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor, view) => {
-				console.log(editor.getSelection())
-				editor.replaceSelection('Sample Editor Command')
-			},
-		})
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: checking => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView)
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open()
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true
-				}
-			},
-		})
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this))
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', evt => {
-			console.log('click', evt)
-		})
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
-		)
+		this.addSettingTab(new MyExportSettingTab(this.app, this))
 	}
 
 	onunload() {}
@@ -87,35 +61,23 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app) {
-		super(app)
-	}
+/**
+ * This is a sample Setting tag
+ */
+class MyExportSettingTab extends PluginSettingTab {
+	plugin: MyPlugin
 
-	onOpen() {
-		const { contentEl } = this
-		contentEl.setText('Woah!')
-	}
-
-	onClose() {
-		const { contentEl } = this
-		contentEl.empty()
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin
-
-	constructor(app, plugin) {
+	constructor(app: App, plugin: MyPlugin) {
 		super(app, plugin)
 		this.plugin = plugin
 	}
 
-	display() {
+	display(): void {
 		const { containerEl } = this
 
 		containerEl.empty()
 
+		// todo add settings here, token, schema etc
 		new Setting(containerEl)
 			.setName('Setting #1')
 			.setDesc("It's a secret")
