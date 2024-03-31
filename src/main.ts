75998ea7e879d3b1ed114c674cf8dc14e9ee9590
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-31 14:21:09 +0200

    ✨ Add ribbon icons and settings handling
    Added functionality to add ribbon icons for image uploading and content generation based on settings. Implemented loading, saving, and processing of plugin settings.

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { Plugin } from 'obsidian'
+import { StrapiExporterSettingTab } from './settings'
+import { DEFAULT_STRAPI_EXPORTER_SETTINGS } from './constants'
+import { processMarkdownContent } from './utils/image-processor'
+
+export default class StrapiExporterPlugin extends Plugin {
+	settings: StrapiExporterSettings
+
+	async onload() {
+		await this.loadSettings()
+
+		// Add ribbon icons and event listeners
+		/**
+		 * Add a ribbon icon to the Markdown view (the little icon on the left side bar)
+		 */
+		const ribbonIconEl = this.addRibbonIcon(
+			'upload',
+			'Upload images to Strapi and update links in Markdown content, then generate article content using OpenAI',
+			async (evt: MouseEvent) => {
+				await this.processMarkdownContent()
+			}
+		)
+		ribbonIconEl.addClass('strapi-exporter-ribbon-class')
+
+		/**
+		 * Add an additional ribbon icon based on the settings
+		 */
+		if (this.settings.enableAdditionalApiCall) {
+			const additionalRibbonIconEl = this.addRibbonIcon(
+				'link',
+				'Upload images to Strapi and update links in Markdown content, then generate additional content using OpenAI',
+				async (evt: MouseEvent) => {
+					await this.processMarkdownContent(true)
+				}
+			)
+			additionalRibbonIconEl.addClass('strapi-exporter-additional-ribbon-class')
+		}
+
+		this.addSettingTab(new StrapiExporterSettingTab(this.app, this))
+	}
+
+	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_STRAPI_EXPORTER_SETTINGS,
+			await this.loadData()
+		)
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings)
+	}
+
+	async processMarkdownContent(useAdditionalCallAPI = false) {
+		// Call processMarkdownContent from image-processor.ts
+		await processMarkdownContent(this.app, this.settings, useAdditionalCallAPI)
+	}
+}
