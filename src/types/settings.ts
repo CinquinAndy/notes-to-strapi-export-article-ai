8c80f72318a33007230244017fefb9a04653d7fb
Cinquin Andy <cinquin.andy@gmail.com>
2024-09-23 12:15:23 +0200

    Update: Refactor settings interface - Added routes and currentTab properties to StrapiExporterSettings interface - Renamed IconConfig interface to RouteConfig for clarity and consistency

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,50 +1,22 @@
-/**
- * The settings for the Strapi Exporter plugin
- */
+// src/types/settings.ts
 export interface StrapiExporterSettings {
 	strapiUrl: string
 	strapiApiToken: string
 	forvoyezApiKey: string
 	openaiApiKey: string
-	jsonTemplate: string
-	jsonTemplateDescription: string
-	strapiArticleCreateUrl: string
-	strapiContentAttributeName: string
-	additionalPrompt: string
-	enableAdditionalApiCall: boolean
-	additionalJsonTemplate: string
-	additionalJsonTemplateDescription: string
-	additionalUrl: string
-	additionalContentAttributeName: string
-	// enable elements
-	mainButtonImageEnabled: boolean
-	mainButtonGalleryEnabled: boolean
-	additionalButtonImageEnabled: boolean
-	additionalButtonGalleryEnabled: boolean
-	// images and galleries paths (for the body api call)
-	mainImageFullPathProperty: string
-	mainGalleryFullPathProperty: string
-	additionalImageFullPathProperty: string
-	additionalGalleryFullPathProperty: string
-	mainRibbonIconTitle: string
-	additionalRibbonIconTitle: string
-	icons: IconConfig[]
 	strapiSchema: string
 	schemaDescription: string
 	generatedConfig: string
-	fieldMappings: {
-		[key: string]: {
-			obsidianField: string
-			transformation: string
-			description: string
-		}
-	}
-	additionalInstructions: string
+	routes: RouteConfig[]
+	currentTab: string
 }
 
-export interface IconConfig {
+export interface RouteConfig {
 	id: string
 	icon: string
-	title: string
+	name: string
+	description: string
 	enabled: boolean
+	url: string
+	language: string
 }
